d65812bb39e90bec4873d4e0834579b742ae3b80
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2020-07-04 20:16:23 +0530

    volley-sample-example: final commit

--- app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
+++ app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
@@ -2,38 +2,78 @@ package com.ebook.kindle.androidmesh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements BottomSheetDialog.BottomSheetListener {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
+    // Instantiated Button View
+    private Button btnRequest;
+
+    //Instantiated RequestQueue, StringRequest
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://run.mocky.io/v3/9c206e41-1ffb-4c8d-88cd-05c1cfda18ac";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonOpenBottomSheet = findViewById(R.id.volley_button);
-        buttonOpenBottomSheet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BottomSheetDialog bottomSheetDialogAsyncTask = new BottomSheetDialog();
-                bottomSheetDialogAsyncTask.show(getSupportFragmentManager(), "VolleyBottomSheet");
-            }
+        //Here we find in the View of Button, that have to be clicked
+        btnRequest = (Button) findViewById(R.id.buttonRequest);
+
+        //Button OnClick is Implemented, while onClick() we are calling the function called sendAndRequestResponse()
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v){
+
+                  sendAndRequestResponse();
+
+              }
         });
+
     }
 
-    @Override
-    public void onTextClicked(String text) {
-        if (text.equals("Java")) {
-            Intent i = new Intent(MainActivity.this, VolleyJavaActivity.class);
-            startActivity(i);
-        }
 
-        /*if (text.equals("Kotlin")) {
-            Intent i = new Intent(MainActivity.this, AsyncTaskKotlinActivity.class);
-            startActivity(i);
-        }*/
+
+    //This the method where we do operation on Toasting the response in our App
+    private void sendAndRequestResponse() {
+
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized, with Params that we are passing our RESTful API url in here, with GET Call
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                //display the response on screen
+                Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
     }
 }
