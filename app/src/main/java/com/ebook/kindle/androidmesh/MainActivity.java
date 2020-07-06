c832b961b0fcee6d708aee768808b806c2f26805
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2020-07-06 13:20:14 +0530

    master branch commit

--- app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
+++ app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
@@ -1,39 +1,17 @@
 package com.ebook.kindle.androidmesh;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements BottomSheetDialog.BottomSheetListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonOpenBottomSheet = findViewById(R.id.volley_button);
-        buttonOpenBottomSheet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                BottomSheetDialog bottomSheetDialogAsyncTask = new BottomSheetDialog();
-                bottomSheetDialogAsyncTask.show(getSupportFragmentManager(), "VolleyBottomSheet");
-            }
-        });
     }
 
-    @Override
-    public void onTextClicked(String text) {
-        if (text.equals("Java")) {
-            Intent i = new Intent(MainActivity.this, VolleyJavaActivity.class);
-            startActivity(i);
-        }
-
-        /*if (text.equals("Kotlin")) {
-            Intent i = new Intent(MainActivity.this, AsyncTaskKotlinActivity.class);
-            startActivity(i);
-        }*/
-    }
 }
