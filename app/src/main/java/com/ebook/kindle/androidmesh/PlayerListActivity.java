446f3622ac7ab1a71040de81d85228ede52e4ec4
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2020-07-09 21:02:13 +0530

    EX01-Volley-GET-RecyclerView TODO stuff done

--- app/src/main/java/com/ebook/kindle/androidmesh/PlayerListActivity.java
+++ app/src/main/java/com/ebook/kindle/androidmesh/PlayerListActivity.java
@@ -2,7 +2,6 @@ package com.ebook.kindle.androidmesh;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 
@@ -10,26 +9,32 @@ public class PlayerListActivity extends AppCompatActivity {
 
     //TODO #7: Create entities as String URL, ProgressDialog, ListView, ArrayList<PlayerModel> and PlayerListAdapter attributes
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_list);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Tennis Players");
+        actionBar.setTitle("Player List");
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         //TODO: Identify ListView as findViewById
 
-        //TODO: Call retrieveJSON() method
+
+        //TODO: Call fetchingJSON() method
+
     }
 
-    //TODO: Create method called retrieveJSON() method, (Follow eBook)
+    //TODO: Create method called fetchingJSON() method, (Follow Book)
 
-    //TODO: Create method called setupListView() method, (Follow eBook)
 
-    //TODO: Create method called showProgressDialog() method, (Follow eBook)
+    //TODO: Create method called setupRecycler() method, (Follow Book)
 
-    //TODO: Create method called removeProgressDialog() method, (Follow eBook)
+
+    //TODO: Create method called showProgressDialog() method, (Follow Book)
+
+
+    //TODO: Create method called removeProgressDialog() method, (Follow Book)
 
 }
