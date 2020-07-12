041b6111b303348f9738acaca487bf708247ab48
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2020-07-12 17:08:52 +0530

    EX05-SOLN-Volley-DELETE solution branch is done

--- app/src/main/java/com/ebook/kindle/androidmesh/DeleteUserActivity.java
+++ app/src/main/java/com/ebook/kindle/androidmesh/DeleteUserActivity.java
@@ -3,17 +3,31 @@ package com.ebook.kindle.androidmesh;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class DeleteUserActivity extends AppCompatActivity {
 
-    /* TODO #4: Create entities like 1 EditText (For User ID)
-     * One Button For Deleting data
-     * One TextView for showing Status Code
-     * Finally, RESTful API URL.
-     */
 
+    private EditText userIDEt;
+    private TextView statusCodeTv;
+    private Button deleteUserBtn;
+
+    private String deleteURL = "https://reqres.in/api/users/";
 
 
     @Override
@@ -25,17 +39,58 @@ public class DeleteUserActivity extends AppCompatActivity {
         actionBar.setTitle("Delete User");
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        userIDEt = findViewById(R.id.user_id_et);
+        statusCodeTv = findViewById(R.id.status_code_tv);
+        deleteUserBtn = findViewById(R.id.delete_user_btn);
 
-        // TODO #5: Identify by findViewById for 1 EditText, 1 button & 1 TextView
 
-
-        //TODO #7: Create setOnClickListener() for button and call deleteUser() method
-
+        deleteUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteUser();
+            }
+        });
 
     }
 
 
-    //TODO #6: Create method called deleteUser(), that performs DELETE-ing data in Volley (Refer Book)
+    public void deleteUser() {
+        final ProgressDialog loading = new ProgressDialog(DeleteUserActivity.this);
+        loading.setMessage("Please Wait...");
+        loading.setCanceledOnTouchOutside(false);
+        loading.show();
+        deleteURL += userIDEt.getText();
 
+        StringRequest deleteRequest = new StringRequest(Request.Method.DELETE, deleteURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        loading.dismiss();
+                        Toast.makeText(DeleteUserActivity.this, "Success", Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        loading.dismiss();
+                        Toast.makeText(DeleteUserActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        )
+        {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                statusCodeTv.setText("Response Status Code: " + response.statusCode);
+                return super.parseNetworkResponse(response);
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(deleteRequest);
+    }
 
 }
