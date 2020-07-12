d17bbaedbbfd8b60ebc1e209066b17fb8954421c
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2020-07-12 17:01:21 +0530

    EX05-Volley-DELETE todos stuff done

--- app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
+++ app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements BottomSheetDialog
     @Override
     public void onTextClicked(String text) {
         if (text.equals("Java")) {
-            Intent i = new Intent(MainActivity.this, UpdateUserActivity.class);
+            Intent i = new Intent(MainActivity.this, DeleteUserActivity.class);
             startActivity(i);
         }
 
