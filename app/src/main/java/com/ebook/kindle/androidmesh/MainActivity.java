57677cb941ddac521d5c32b63dc637c594a30069
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2020-07-14 13:44:17 +0530

    EG02-Retrofit-Sample template done

--- app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
+++ app/src/main/java/com/ebook/kindle/androidmesh/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity implements BottomSheetDialog
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonOpenBottomSheet = findViewById(R.id.volley_button);
+        Button buttonOpenBottomSheet = findViewById(R.id.retrofit_button);
         buttonOpenBottomSheet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements BottomSheetDialog
     @Override
     public void onTextClicked(String text) {
         if (text.equals("Java")) {
-            Intent i = new Intent(MainActivity.this, DeleteUserActivity.class);
+            Intent i = new Intent(MainActivity.this, PostActivity.class);
             startActivity(i);
         }
 
